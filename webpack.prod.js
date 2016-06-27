959d34799ad5bc723db40676b88772f4e3cddbc4
Adam Stankiewicz <sheerun@sher.pl>
2016-06-27 23:01:52 +0200

    Production build

---
+++ webpack.prod.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'ping'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+};
